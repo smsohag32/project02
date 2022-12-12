e2c0d8d64d0406ca6a98086fcd76c868249f7add
SM Sohag <sohagsheik32@gmail.com>
2022-12-12 16:49:14 +0600

    firt commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing
+var typed = new Typed(".typing", {
+    strings: ["Web Developer.", "Data Analyst.", "Web Researcher.", "Web Designer."],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+});
